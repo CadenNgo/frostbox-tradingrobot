1baa29602cf6d55503264a0281494de8288cba40
“Frostbox” <weianngo@gmail.com>
2025-04-21 21:36:40 +0800

    first commit

---
+++ src/main/java/com/frostbox/tradingrobot/frostbox_tradingrobot/FrostboxTradingrobotApplication.java
@@ -0,0 +1,13 @@
+package com.frostbox.tradingrobot.frostbox_tradingrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrostboxTradingrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrostboxTradingrobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frostbox/tradingrobot/frostbox_tradingrobot/FrostboxTradingrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frostbox.tradingrobot.frostbox_tradingrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrostboxTradingrobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
